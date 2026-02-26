80e6bc593a9c1eab6df75175c1a598605fa39d2e
ConnerLooney <tca6ng@virginia.edu>
2026-02-26 13:03:32 -0500

    Update data_release_1_main.py

--- data_release_1_main.py
+++ data_release_1_main.py
@@ -10,13 +10,20 @@ import pandas as pd
 
 #goal is to make a scatter plot showing day vs active infections 
 
-with open(r"C:\Users\dance\OneDrive - University of Virginia\Computational BME\Module-2-Epidemics-SIR-Modeling-Broderick_Looney\Data\mystery_virus_daily_active_counts_RELEASE#1.csv", newline="") as f: #opening the csv file to get the headers of the csv file
+#with open(r"C:\Users\dance\OneDrive - University of Virginia\Computational BME\Module-2-Epidemics-SIR-Modeling-Broderick_Looney\Data\mystery_virus_daily_active_counts_RELEASE#1.csv", newline="") as f: #opening the csv file to get the headers of the csv file
+    #reader = csv.reader(f)
+    #headers = next(reader) # Get the first row to get the headers of the csv
+    #for h in headers:
+     #   print(h) #show the headers of the csv
+
+with open("/Users/connerlooney/Documents/GitHub/Module-2-Epidemics-SIR-Modeling-Broderick_Looney/Data/mystery_virus_daily_active_counts_RELEASE#1.csv", newline="") as f: #opening the csv file to get the headers of the csv file
     reader = csv.reader(f)
     headers = next(reader) # Get the first row to get the headers of the csv
     for h in headers:
-        print(h) #show the headers of the csv
+        print(h) #show the headers of the csv) as f: #opening the csv file to get the headers of the csv file
 
-Virus_count.instantiate_from_csv(r"C:\Users\dance\OneDrive - University of Virginia\Computational BME\Module-2-Epidemics-SIR-Modeling-Broderick_Looney\Data\mystery_virus_daily_active_counts_RELEASE#1.csv") #instantiating virus_count objects from the csv file using the class method instantiate_from_csv
+#Virus_count.instantiate_from_csv(r"C:\Users\dance\OneDrive - University of Virginia\Computational BME\Module-2-Epidemics-SIR-Modeling-Broderick_Looney\Data\mystery_virus_daily_active_counts_RELEASE#1.csv")
+Virus_count.instantiate_from_csv(r"/Users/connerlooney/Documents/GitHub/Module-2-Epidemics-SIR-Modeling-Broderick_Looney/Data/mystery_virus_daily_active_counts_RELEASE#1.csv")#instantiating virus_count objects from the csv file using the class method instantiate_from_csv
 
 day = [] #creating a list to store the day of each virus_count object
 active_reported_daily_cases = [] #creating a list to store the active reported daily cases of each virus_count object
