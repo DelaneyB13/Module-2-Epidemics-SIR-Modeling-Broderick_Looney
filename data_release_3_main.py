a16c1be1f2dd44de2178d9efbbcfc7dd63b721c7
DelaneyB13 <fju4wf@virginia.edu>
2026-03-12 14:24:32 -0400

    update

--- data_release_3_main.py
+++ data_release_3_main.py
@@ -11,20 +11,20 @@ import pandas as pd
 
 #goal is to make a scatter plot showing day vs active infections 
 
-#with open(r"C:\Users\dance\OneDrive - University of Virginia\Computational BME\Module-2-Epidemics-SIR-Modeling-Broderick_Looney\Data\mystery_virus_daily_active_counts_RELEASE#1.csv", newline="") as f: #opening the csv file to get the headers of the csv file
-    #reader = csv.reader(f)
-    #headers = next(reader) # Get the first row to get the headers of the csv
-    #for h in headers:
-     #   print(h) #show the headers of the csv
-
-with open("/Users/connerlooney/Documents/GitHub/Module-2-Epidemics-SIR-Modeling-Broderick_Looney/Data/mystery_virus_daily_active_counts_RELEASE#2.csv", newline="") as f: #opening the csv file to get the headers of the csv file
+with open(r"C:\Users\dance\OneDrive - University of Virginia\Computational BME\Module-2-Epidemics-SIR-Modeling-Broderick_Looney\Data\mystery_virus_daily_active_counts_RELEASE#1.csv", newline="") as f: #opening the csv file to get the headers of the csv file
     reader = csv.reader(f)
     headers = next(reader) # Get the first row to get the headers of the csv
     for h in headers:
-       print(h) #show the headers of the csv
+        print(h) #show the headers of the csv
 
-#Virus_count.instantiate_from_csv(r"C:\Users\dance\OneDrive - University of Virginia\Computational BME\Module-2-Epidemics-SIR-Modeling-Broderick_Looney\Data\mystery_virus_daily_active_counts_RELEASE#2.csv")
-Virus_count.instantiate_from_csv(r"/Users/connerlooney/Documents/GitHub/Module-2-Epidemics-SIR-Modeling-Broderick_Looney/Data/mystery_virus_daily_active_counts_RELEASE#2.csv")#instantiating virus_count objects from the csv file using the class method instantiate_from_csv
+#with open("/Users/connerlooney/Documents/GitHub/Module-2-Epidemics-SIR-Modeling-Broderick_Looney/Data/mystery_virus_daily_active_counts_RELEASE#2.csv", newline="") as f: #opening the csv file to get the headers of the csv file
+    #reader = csv.reader(f)
+    #headers = next(reader) # Get the first row to get the headers of the csv
+    #for h in headers:
+       #print(h) #show the headers of the csv
+
+Virus_count.instantiate_from_csv(r"C:\Users\dance\OneDrive - University of Virginia\Computational BME\Module-2-Epidemics-SIR-Modeling-Broderick_Looney\Data\mystery_virus_daily_active_counts_RELEASE#2.csv")
+#Virus_count.instantiate_from_csv(r"/Users/connerlooney/Documents/GitHub/Module-2-Epidemics-SIR-Modeling-Broderick_Looney/Data/mystery_virus_daily_active_counts_RELEASE#2.csv")#instantiating virus_count objects from the csv file using the class method instantiate_from_csv
 
 day = [] #creating a list to store the day of each virus_count object
 active_reported_daily_cases = [] #creating a list to store the active reported daily cases of each virus_count object
@@ -103,42 +103,78 @@ def seir(day,beta,sigma,gamma,S0,E0,I0,R0,N,h):
         R_list.append(R_list[virus_day] + h * dR)
 
     return S_list, E_list, I_list, R_list
-data = pd.read_csv("/Users/connerlooney/Documents/GitHub/Module-2-Epidemics-SIR-Modeling-Broderick_Looney/Data/mystery_virus_daily_active_counts_RELEASE#2.csv")
-#data = pd.read_csv(r"C:\Users\dance\OneDrive - University of Virginia\Computational BME\Module-2-Epidemics-SIR-Modeling-Broderick_Looney\Data\mystery_virus_daily_active_counts_RELEASE#2.csv")
+#data = pd.read_csv("/Users/connerlooney/Documents/GitHub/Module-2-Epidemics-SIR-Modeling-Broderick_Looney/Data/mystery_virus_daily_active_counts_RELEASE#2.csv")
+data = pd.read_csv(r"C:\Users\dance\OneDrive - University of Virginia\Computational BME\Module-2-Epidemics-SIR-Modeling-Broderick_Looney\Data\mystery_virus_daily_active_counts_RELEASE#2.csv")
 data = data["active reported daily cases"].tolist()
 
-def grid_search(day,N,S0,E0,I0,R0,data):
-    beta = np.linspace(0.2, 0.7)
-    sigma = np.linspace(0.1, 0.3)
-    gamma = np.linspace(0.05, 0.25)
-    SSE = []
+def grid_search(day, N, S0, E0, I0, R0, data):
+    # parameter grids
+    beta_values = np.linspace(0.2, 0.7)
+    sigma_values = np.linspace(0.1, 0.3)
+    gamma_values = np.linspace(0.05, 0.25)
+
+    # step size for Euler
+    h = 1
+
+    # storage for search results
+    SSE_list = []
     beta_list = []
     sigma_list = []
     gamma_list = []
-    for b in beta:
-        for s in sigma:
-            for g in gamma:
-                seir(day,b,s,g,S0,E0,I0,R0,N,h)
+
+
+    best_SSE = float("inf")
+    best_beta = None
+    best_sigma = None
+    best_gamma = None
+
+    # iterate through parameter combinations
+    for b in beta_values:
+        for s in sigma_values:
+            for g in gamma_values:
+                # run Euler method for these parameters
+                S_list, E_list, I_list, R_list = seir(day, b, s, g, S0, E0, I0, R0, N, h)
+
+                model_I = I_list[: len(data)]
+
+                # compute sum of squared errors
+                errors = [(model_I[i] - data[i]) ** 2 for i in range(len(model_I))]
+                sse = sum(errors)
+
+                # record results
+                SSE_list.append(sse)
                 beta_list.append(b)
                 sigma_list.append(s)
                 gamma_list.append(g)
 
+                # update best if this is lowest SSE so far
+                if sse < best_SSE:
+                    best_SSE = sse
+                    best_beta = b
+                    best_sigma = s
+                    best_gamma = g
+    
+    return best_beta, best_sigma, best_gamma, best_SSE, SSE_list, beta_list, sigma_list, gamma_list
 
+# perform grid search using defined parameters
+best_beta, best_sigma, best_gamma, best_SSE, SSE_list, beta_list, sigma_list, gamma_list = \
+    grid_search(day, N, S0, E0, I0, R0, data)
 
+# print out best parameters and associated SSE
+print(f"Best parameters from grid search:\n  beta = {best_beta}\n  sigma = {best_sigma}\n  gamma = {best_gamma}\n  SSE = {best_SSE}")
 
+# generate model output with optimal parameters
+S_list_opt, E_list_opt, I_list_opt, R_list_opt = seir(day, best_beta, best_sigma, best_gamma, S0, E0, I0, R0, N, h=1)
 
-
-     
-                
-'''
-plt.plot(day, S_list, label="Susceptible")
-plt.plot(day, E_list, label="Exposed")
-plt.plot(day, I_list, label="Infectious")
-plt.plot(day, R_list, label="Recovered")
+# plot the optimal SEIR curves
+plt.plot(range(day+1), S_list_opt, label="Susceptible")
+plt.plot(range(day+1), E_list_opt, label="Exposed")
+plt.plot(range(day+1), I_list_opt, label="Infectious")
+plt.plot(range(day+1), R_list_opt, label="Recovered")
 
 plt.xlabel("Day")
 plt.ylabel("Population")
-plt.title("SEIR Model")
+plt.title("SEIR Model (best fit)")
 plt.legend()
 
-plt.show()'''
+plt.show()
